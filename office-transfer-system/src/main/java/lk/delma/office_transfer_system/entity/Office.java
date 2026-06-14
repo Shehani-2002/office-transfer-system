a562660a618d4be310db533cfaca404a313c5129
Shehani-2002 <shehanibhagya755@gmail.com>
2026-06-14 16:31:11 +0530

    Initial commit - Office Transfer System

---
+++ office-transfer-system/src/main/java/lk/delma/office_transfer_system/entity/Office.java
@@ -0,0 +1,4 @@
+package lk.delma.office_transfer_system.entity;
+
+public class Office {
+}
---
+++ office-transfer-system/src/test/java/lk/delma/office_transfer_system/OfficeTransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.delma.office_transfer_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficeTransferSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
